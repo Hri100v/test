e174ebc278c1ce55d2312f8ce9a985a1907daccc
Hri100v <hr100h@gmail.com>
2015-01-21 17:08:05 +0200

    update some test
    problem 07

---
+++ 07 Problem - Quotes in Strings/charQSu0022.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Problem 7. Quotes in Strings
+
+    Declare two string variables and assign them with following value: The "use" of quotations causes difficulties.
+    Do the above in two different ways - with and without using quoted strings.
+    Print the variables to ensure that their value was correctly defined.
+ */
+
+using System;
+
+    class explicitString
+    {
+        static void Main()
+        {
+            Console.WriteLine("Salute");
+            Console.WriteLine();
+
+            string str1 = "The \"use\" of quotations causes difficulties";
+            Console.WriteLine(str1);
+
+            string str2 = @"The ""use"" of quotations causes difficulties";
+            Console.WriteLine(str2);
+
+            char quotedStr = '\u0022';
+            string str3 = @"The {0}use{0} of quotations causes difficulties";
+            Console.WriteLine(str3, quotedStr);
+        }
+    }
